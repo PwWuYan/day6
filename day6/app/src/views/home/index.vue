5ccdaa61c93bde05305666420946cc48e46bf3ad
wuyan <1319775253@qq.com>
2019-09-06 09:09:01 +0800

    day6

---
+++ day6/app/src/views/home/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="list">
+      <dl v-for="(item,index) in list" :key="index">
+          <dt>♥</dt>
+          <dd>{{item.tit}}</dd>
+      </dl>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            list:[
+                {
+                    tit:'成单奖励'
+                },{
+                    tit:'成单奖励'
+                },{
+                    tit:'成单奖励'
+                },{
+                    tit:'成单奖励'
+                },{
+                    tit:'成单奖励'
+                },{
+                    tit:'成单奖励'
+                },{
+                    tit:'成单奖励'
+                },{
+                    tit:'成单奖励'
+                }
+            ]
+        }
+    },
+}
+</script>
+
+<style lang='scss' scoped>
+    .list{
+        width: 100%;
+        margin: 50px 0;
+        display: flex;
+         flex-wrap: wrap;
+        dl{
+            width: 25%; 
+            display: flex;
+            flex-direction: column;
+           
+            dt{
+                text-align: center;
+            }
+            dd{
+                text-align: center;
+                font-size: 12px;
+            }
+        }
+    }
+
+</style>
---
+++ day6/app/src/views/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="box">
+      <header>
+          <swiper :options="swiperOption" ref="mySwiper" @someSwiperEvent="callback">
+              <swiper-slide><img src="/1.jpg" alt=""></swiper-slide>
+              <swiper-slide><img src="/2.jpg" alt=""></swiper-slide>
+              <swiper-slide><img src="/3.jpg" alt=""></swiper-slide>
+              <swiper-slide><img src="/4.jpg" alt=""></swiper-slide>
+               <swiper-slide><img src="/5.jpg" alt=""></swiper-slide>
+              <swiper-slide><img src="/6.jpg" alt=""></swiper-slide>
+              <swiper-slide><img src="/7.jpg" alt=""></swiper-slide>
+          </swiper>
+      </header>
+      <div class="main">
+          <router-view></router-view>
+      </div>
+      <footer>
+          <router-link to="/home">首页</router-link>
+          <router-link to="/my">我的</router-link>
+      </footer>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"carrousel",
+    data(){
+        return {
+            swiperOption:{
+                autoplay:true,
+                loop:true
+            }
+        }
+    }
+}
+</script>
+
+<style lang='scss' scoped>
+    .box{
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+    }
+    header{
+        width: 100%;
+        height: 260px;
+        
+        
+    }
+    .main{
+        flex: 1
+    }
+    footer{
+        width: 100%;
+        height: 50px;
+        display: flex;
+        a{
+            flex: 1;
+            display: inline-block;
+            text-align: center;
+            line-height: 50px;
+            color:white;
+        }
+    }
+</style>
